838d686303c78236415bcda46e739cb68b1f9002
Justin Chang <justin.p.chang@gmail.com>
2015-08-08 18:58:33 -0400

    Initial Commit
    Basic libGDX game framework

---
+++ src/desktop/src/ua/at/zenum/ballherders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ua.at.zenum.ballherders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ua.at.zenum.ballherders.BallHerders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BallHerders(), config);
+	}
+}
